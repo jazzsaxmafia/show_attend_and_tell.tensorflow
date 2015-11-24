559567569184952a8e47dc55d4b15316131cff10
taeksoo <jazzsaxmafia@gmail.com>
2015-11-24 23:33:14 +0900

    이어서 학습하는 부분 관련해 수정

--- model_tensorflow.py
+++ model_tensorflow.py
@@ -256,12 +256,11 @@ def train(pretrained_model_path=pretrained_model_path): # 전에 학습하던게
     loss, context, sentence, mask = caption_generator.build_model()
     saver = tf.train.Saver(max_to_keep=50)
 
+    train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+    tf.initialize_all_variables().run()
     if pretrained_model_path is not None:
         print "Starting with pretrained model"
         saver.restore(sess, pretrained_model_path)
-
-    train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
-    tf.initialize_all_variables().run()
 
     index = list(annotation_data.index)
     np.random.shuffle(index)
